393efd7c756820e4debd152690e3548dcd657a2d
arnaudbrubacher <76607875+arnaudbrubacher@users.noreply.github.com>
2025-02-19 00:06:38 -0500

    5.3.2 login works!

--- src/models/user.go
+++ src/models/user.go
@@ -10,7 +10,6 @@ type User struct {
 	Name     string    `json:"name"`
 	Email    string    `gorm:"unique" json:"email"`
 	Password string    `json:"password"`
-	Role     string    `json:"role"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
